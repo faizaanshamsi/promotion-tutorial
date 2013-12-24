2b5b5a3600a275f8cef3fe18b04dc8cdeabcd03c
faizaanshamsi <faizaanshamsi@gmail.com>
2013-12-24 11:50:39 -0500

    create about promotion screen

---
+++ app/screens/about_pro_motion_screen.rb
@@ -0,0 +1,21 @@
+class AboutProMotionScreen < ProMotion::Screen
+  title 'About ProMotion'
+
+  def will_appear
+  self.view.backgroundColor = UIColor.darkGrayColor
+  add UILabel.alloc.initWithFrame(CGRectMake(25, 50, 275, 150)), {
+    text: "ProMotion is a new way to easily organize and develop RubyMotion apps using the concept of screens.",
+    border_style: UITextBorderStyleRoundedRect,
+    background_color: UIColor.whiteColor,
+    font: UIFont.systemFontOfSize(14),
+    number_of_lines: 0,
+    line_break_mode: UILineBreakModeWordWrap,
+    layer: {
+      border_width: 5,
+      corner_radius: 15,
+      border_color: UIColor.grayColor
+      }
+    }
+  end
+
+end
--- app/screens/home_screen.rb
+++ app/screens/home_screen.rb
@@ -1,11 +1,12 @@
 class HomeScreen < ProMotion::GroupedTableScreen
 
+  title 'ProMotion Tutorial'
   def table_data
     [{
       title: 'ProMotion',
       cells: [
         { title: 'About ProMotion', action: :about_promotion },
-        { title: 'About Jamon', action: :about_jammon }
+        { title: 'About Jamon', action: :about_jamon }
       ]
     }, {
       title: 'Help',
@@ -15,4 +16,21 @@ class HomeScreen < ProMotion::GroupedTableScreen
         ]
       }]
   end
+
+  def about_promotion
+    open AboutProMotionScreen
+  end
+
+  def about_jamon
+    p "About Jamon tapped!"
+  end
+
+  def support
+    p "Support tapped!"
+  end
+
+  def feedback
+    p "Feedback tapped!"
+  end
+
 end
